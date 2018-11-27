553d2fd13bf2f0076d98f5eaba49981896737d9c
unknown <chenxy@game-reign.com>
2018-11-27 16:56:24 +0800

    [List-Fix-Bug-InReverseKGroup]

--- list/ReverseKGroup.java
+++ list/ReverseKGroup.java
@@ -1,35 +1,35 @@
 package leetcode.list;
 
+
 public class ReverseKGroup {
+
+    private ListNode reverseNodes(ListNode start, ListNode end) {
+        ListNode curStart = start.next;
+        ListNode pre = start.next;
+        ListNode t = start.next.next;
+        while(t != end) {
+            ListNode nex = t.next;
+            t.next = pre;
+            pre = t;
+            t = nex;
+        }
+        start.next = pre;
+        curStart.next = end;
+        return curStart;
+    }
 
     public ListNode reverseKGroup(ListNode head, int k) {
         int i = 0;
-        ListNode t = head;
-        ListNode oldH = head;
-        ListNode oldT = null;
-        ListNode pre = null;
+        ListNode node = new ListNode(0);
+        node.next = head;
+        head = node;
+        ListNode t = head.next;
+        ListNode start = head;
         while(t != null) {
-            if(i == 0) {
-                oldT = t;
-                pre = t;
-                t = t.next;
+            t = t.next;
+            if(i == k - 1) {
+                start = reverseNodes(start, t);
             }
-            else {
-                ListNode nex = t.next;
-                t.next = pre;
-                pre = t;
-                if(i == k - 1) {
-                    oldT.next = nex;
-                    if(oldH == head) {
-                        head = t;
-                    } else {
-                        oldH.next = t;
-                    }
-                    oldH = oldT;
-                }
-                t = nex;
-            }
-
             i = (i + 1) % k;
         }
         return head;
